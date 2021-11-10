116f7e3cba4a8fb10879112edc93b7f477e84d6d
huuhoa1 <nico35.hoa@gmail.com>
2021-11-10 14:41:27 -0500

    1st commit

---
+++ src/main/java/com/codeforgeyt/artemisdemo/service/ReceiverService.java
@@ -0,0 +1,19 @@
+package com.codeforgeyt.artemisdemo.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReceiverService {
+
+    Logger log = LoggerFactory.getLogger(ReceiverService.class);
+
+//    @JmsListener(destination = "${jms.queue2}", containerFactory = "topicConnectionFactory")
+    @JmsListener(destination = "${jms.queue2}")
+    public void receiveMessage(String message){
+        log.info("Received message: " + message);
+    }
+    
+}
